320f6069656b027e135014e66367579df26f685c
ge64qij <boris.trifonov@tum.de>
2023-09-27 16:09:18 +0200

    changes to nginx and vite config

--- app/services/frontend/vite.config.js
+++ app/services/frontend/vite.config.js
@@ -7,7 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: "/boris_frontend/",
-  publicDir: "public",
+  publicDir: "public", 
   resolve: { 
     alias: {
        '@': './src',
