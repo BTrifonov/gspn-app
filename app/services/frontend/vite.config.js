c8e5f1fcad17c3bd44c70ae60da968bc5ff0c3b3
ge64qij <boris.trifonov@tum.de>
2023-09-27 16:08:03 +0200

    Changed docker config and nginx config

--- app/services/frontend/vite.config.js
+++ app/services/frontend/vite.config.js
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   base: "/boris_frontend/",
+  publicDir: "public",
   resolve: { 
     alias: {
        '@': './src',
